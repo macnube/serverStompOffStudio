a6444017badb96811ec8753aa1297c1905b943ff
Paul McCloud <paul.mccloud@gmail.com>
2019-04-14 20:25:14 +0200

    First commit

---
+++ generated/prisma-client/index.js
@@ -0,0 +1,65 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "User",
+    embedded: false
+  },
+  {
+    name: "Student",
+    embedded: false
+  },
+  {
+    name: "Class",
+    embedded: false
+  },
+  {
+    name: "Attendee",
+    embedded: false
+  },
+  {
+    name: "Role",
+    embedded: false
+  },
+  {
+    name: "ClassStudent",
+    embedded: false
+  },
+  {
+    name: "Teacher",
+    embedded: false
+  },
+  {
+    name: "ClassInstance",
+    embedded: false
+  },
+  {
+    name: "Studio",
+    embedded: false
+  },
+  {
+    name: "Room",
+    embedded: false
+  },
+  {
+    name: "Card",
+    embedded: false
+  },
+  {
+    name: "PaymentType",
+    embedded: false
+  },
+  {
+    name: "Payment",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `http://localhost:4466`
+});
+exports.prisma = new exports.Prisma();
---
+++ index.js
@@ -0,0 +1,46 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const { prisma } = require('./generated/prisma-client');
+const { ApolloServer, gql } = require('apollo-server');
+const { importSchema } = require('graphql-import');
+
+const resolvers = {
+    Query: {
+        users(root, args, context) {
+            return context.prisma.users();
+        },
+    },
+    Mutation: {
+        createUser(root, args, context) {
+            return context.prisma.createUser({
+                name: args.name,
+                email: args.email,
+                password: args.password,
+                admin: ars.admin,
+            });
+        },
+        createStudent(root, args, context) {
+            return context.prisma.createStudent({
+                name: args.name,
+                email: args.email,
+                mobile: args.mobile,
+            });
+        },
+    },
+};
+
+const server = new ApolloServer({
+    typeDefs: gql(importSchema('./schema.graphql')),
+    resolvers,
+    context: {
+        prisma,
+    },
+    introspection: true,
+});
+
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+    console.log(`🚀  Prisma ready at ${process.env.PRISMA_ENDPOINT}`);
+});
